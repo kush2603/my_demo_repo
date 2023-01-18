86fa7f45386f29d12fcc8752cba2ee406618c78e
kush2603 <kushawaha.sandeep@gmail.com>
2023-01-18 19:47:56 +0530

    hi,team
    plz review my code.

---
+++ SampleProject/src/newPackage/ThisKeyword.java
@@ -0,0 +1,21 @@
+package newPackage;
+
+public class ThisKeyword {
+	
+	public void fruit()
+	{
+		System.out.println("i'm in fruit method");
+	}
+	
+	public void vegetables()
+	{
+		this.fruit();
+		System.out.println("i'm in vegetables");
+	}
+
+	public static void main (String [ ] args)
+	{
+		ThisKeyword t = new ThisKeyword();
+		t.vegetables();
+	}
+}
