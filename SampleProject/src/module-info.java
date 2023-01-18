c0fed60e241adfef00a13c31f2ebcdd9f68eeb2c
kush2603 <kushawaha.sandeep@gmail.com>
2023-01-18 13:11:28 +0530

    Basic Task
    Hi,
    I've completed basic Task.
    Thanks

---
+++ SampleProject/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Sandeep Kushawaha
+ *
+ */
+module SampleProject {
+}
---
+++ SampleProject/src/my_package/TestBase.java
@@ -0,0 +1,19 @@
+package my_package;
+
+public class TestBase {
+
+	public void myMethod()
+	{
+		System.out.println("My First TestBase Method in GIT REPOSITORY project");
+	}
+	public static void main(String[] args) {
+		
+		TestBase t = new TestBase();
+		t.myMethod();
+	
+		
+		
+
+	}
+
+}
